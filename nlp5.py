53ca900d77c9ee24153802bc4da7da6c3dc8c60d
Jonbugge4 <jonbugge4@gmail.com>
2021-11-01 15:05:16 -0500

    more nlp work

--- nlp5.py
+++ nlp5.py
@@ -1,6 +1,5 @@
 from wordcloud import WordCloud
 import imageio
-import spacy
 import textblob
 from pathlib import Path
 import pandas as pd
@@ -8,22 +7,38 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from textblob import TextBlob
-stops = stopwords.words('english')
 
+stops = stopwords.words('english') 
+
+additional =['thy', 'ye', 'verily', 'thee', 'hath', 
+'say', 'thou', 'art', 'shall']
+
+
+stops += additional
 
 text = Path('book of John text.txt').read_text()
 
+
 blob = TextBlob(text)
 
-print(blob)
+#print(blob)
 
-df = pd.DataFrame(text, columns = ['word', 'count'])
-print(df)
+from operator import itemgetter
+
+items = blob.noun_phrases
+
+items = [item for item in items if item[0] not in stops]
+
+sorted_items = sorted(items, key = itemgetter(1), reverse = True)
+
+top15 =sorted_items[1:16]
+
+df = pd.DataFrame(top15, columns=['word', 'count'])
 
 mask_image = imageio.imread('mask_circle.png')
 
 wordcloud = WordCloud(colormap='prism', mask = mask_image, background_color='white')
-print('test')
+print(df)
 '''
 wordcloud = wordcloud.generate(text)
 
